1a4df418e1a1b4401d6f2f0b9022ae03cb000a82
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2021-11-16 16:55:01 -0300

    Adicionando as funções do CreateMenu

--- restaurant/script.js
+++ restaurant/script.js
@@ -7,6 +7,42 @@ const plus2 = document.querySelectorAll('.plus')[1];
 const input1 = document.querySelector('input');
 const menuBtn = document.querySelector('.menu-btn');
 
+// funções geradas no projeto JS-Unit-Tests
+const restaurant = {
+  fetchMenu: () => {},
+  consumption: [],
+};
+
+const addOrder = (myOrder) => {
+  restaurant.consumption.push(myOrder);
+};
+
+const sumKey = (menuKey) => {
+  let actualSum = 0;
+  for (let i = 0; i < restaurant.consumption.length; i += 1) {
+    const element = restaurant.consumption[i];
+    if (menuKey[element]) {
+      actualSum += menuKey[element];
+    }
+  }
+  return actualSum * 1.1;
+};
+
+const createMenu = (myMenu) => {
+  restaurant.fetchMenu = () => myMenu;
+  restaurant.order = addOrder;
+  restaurant.pay = () => {
+    let actualSum = 0;
+    for (let index = 0; index < Object.keys(myMenu).length; index += 1) {
+      let keyArr = Object.values(myMenu);
+      actualSum += sumKey(keyArr[index]);
+    }
+    return actualSum;
+  };
+  return restaurant;
+};
+// funções acima foram geradas no projeto JS-Unit-Tests
+
 const changeFood = () => {;
   const lastSib = plus1.previousSibling;
   parentL1.lastChild.remove();
