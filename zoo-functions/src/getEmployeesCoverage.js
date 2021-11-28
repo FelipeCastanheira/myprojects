ac73f269a9882dd295b35c254fdfeb3d7af304c2
Felipe Castanheira Carvalho <castanheirafc2@gmail.com>
2021-11-27 21:07:03 -0300

    Update getEmployees

--- zoo-functions/src/getEmployeesCoverage.js
+++ zoo-functions/src/getEmployeesCoverage.js
@@ -1,5 +1,6 @@
 const data = require('../data/zoo_data');
 
+// o parâmetro info é um objeto com todas as informações do employee
 const getAnswerInfo = (info) => ({
   id: info.id,
   fullName: `${info.firstName} ${info.lastName}`,
@@ -11,6 +12,7 @@ const getAnswerInfo = (info) => ({
     .map((spec) => spec.location),
 });
 
+// sabendo que o parâmetro é um id, é usada a função ansById
 const ansById = (id) => {
   if (data.employees.some((person) => person.id === id)) {
     const info = data.employees.find((person) => person.id === id);
@@ -19,6 +21,7 @@ const ansById = (id) => {
   throw new Error('Informações inválidas');
 };
 
+// função principal
 function getEmployeesCoverage(emplOject) {
   if (!emplOject) {
     return data.employees.map((info) => getAnswerInfo(info));
